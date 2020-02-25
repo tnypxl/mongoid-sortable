aa6d146819cfcd390d5225f26330473cd5f037d1
Arik Jones <tnypxl@Ariks-MacBook-Pro.local>
2020-02-25 16:10:09 -0600

    Cleaning up code based on rubocop/solargraph

--- lib/mongoid-sortable/version.rb
+++ lib/mongoid-sortable/version.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module Mongoid
   module Sortable
-    VERSION = "1.0.0"
+    VERSION = '1.0.0'
   end
 end
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
 require 'factory_bot'
 
 FactoryBot.define do
-	factory :parent_document do
-		trait :with_documents do
-			after(:create) do |parent, evaluator|
-				3.times { FactoryBot.create(:document, parent_document: parent) }
-			end
-		end
-	end
+  factory :parent_document do
+    trait :with_documents do
+      after(:create) do |parent, _evaluator|
+        3.times { FactoryBot.create(:document, parent_document: parent) }
+      end
+    end
+  end
 
-	factory :document do
-		trait :with_embedded_docs do
-			after(:create) do |parent, evaluator|
-				3.times { FactoryBot.create(:embedded_document, document: parent) }
-			end
-		end
-	end
+  factory :document do
+    trait :with_embedded_docs do
+      after(:create) do |parent, _evaluator|
+        3.times { FactoryBot.create(:embedded_document, document: parent) }
+      end
+    end
+  end
 
-	factory :embedded_document
+  factory :embedded_document
 end
